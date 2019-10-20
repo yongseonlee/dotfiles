8b804595d573d26ba90cddcf5b2d8c37c53fdb6e
Yongseon Lee <yongseonlee0@gmail.com>
2019-10-21 01:38:22 +0900

    Add homebrew setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 
+if [[ $1 == "--brew" ]]; then
+    if [[ ! command -v brew >/dev/null 2>&1 ]]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew tap caskroom/cask
+    brew tap homebrew/cask-fonts
+    brew cask install $(cat .homebrew/casks)
+    brew install $(cat .homebrew/formulae)
+    $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+fi
+
+
 for file in $(ls -a); do
     if [[ $file == karabiner.json ]]; then
         mkdir -p ~/.config/karabiner

5dff0fcf2cac6804075540d238621c295cf7cea9
Yongseon Lee <yongseonlee0@gmail.com>
2019-10-21 00:33:07 +0900

    Add KeyBindings and karabiner.json

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+
 for file in $(ls -a); do
-    if [[ ! $file =~ ^\.+ && $file != $0 ]]; then
+    if [[ $file == karabiner.json ]]; then
+        mkdir -p ~/.config/karabiner
+        cp $file ~/.config/karabiner/
+    elif [[ $file == KeyBindings ]]; then
+        cp -r $file ~/Library/
+    elif [[ ! $file =~ ^\.+ && $file != $0 ]]; then
         cp -r $file ~/.$file
     fi
 done

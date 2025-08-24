3e29145756082e26453f66ef1cc762155bffc3f7
Yongseon Lee <yongseonlee0@gmail.com>
2025-08-24 16:11:10 +0900

    Tidy packages

--- setup.sh
+++ setup.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/cask-fonts
-brew cask install $(cat .homebrew/casks)
-brew install $(cat .homebrew/formulae)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install $(cat packages/homebrew)
+mise use -g $(cat packages/mise)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
-for file in $(ls -a); do
-    if [[ $file == karabiner.json ]]; then
-        mkdir -p ~/.config/karabiner
-        cp $file ~/.config/karabiner/
-    elif [[ $file == KeyBindings ]]; then
-        cp -r $file ~/Library/
-    elif [[ ! $file =~ ^\.+ && $file != $0 ]]; then
-        cp -r $file ~/.$file
-    fi
+for file in dotfiles/*; do
+    filename=$(basename "$file")
+    cp -r "$file" ~/."$filename"
 done
+
+if [[ -f karabiner.json ]]; then
+    mkdir -p ~/.config/karabiner
+    cp karabiner.json ~/.config/karabiner/
+fi
+
+if [[ -d KeyBindings ]]; then
+    cp -r KeyBindings ~/Library/
+fi
 
 git config --global core.excludesfile ~/.gitignore_global
 $(brew --prefix)/opt/fzf/install
+
